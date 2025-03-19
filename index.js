7c64c2e3eb3abe3ed00786254305020a2ba10572
angep73 <p.umunyana@alustudent.com>
2025-03-19 16:12:11 +0200

    adding models in my database

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Product = require('./models/product.model');
+
+
+
+
+
+
 mongoose.connect("mongodb+srv://angep32:eVE7wc6TBEFy0AMs@summativeapi.6s4pw.mongodb.net/").then(()=>{
     console.log('Connected to MongoDB');
 }).catch(
     (err)=>console.error('Could not connect to MongoDB',err)
 )
 
-//yPabozushDlBEkTy
-//eVE7wc6TBEFy0AMs
+app.post('/products',async (req,res)=>{
+    try {
+        const product = await Product.create(req.body);
+        res.status(201).json(product);
+
+        
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000,"localhost",()=>{
     console.log('Server is running on port 3000');
 })
 
-app.get('/',(req,res)=>{
-    res.send('Hello World');
-})
--- models/product.model.js
+++ models/product.model.js
@@ -33,3 +33,7 @@ const ProductSchema = mongoose.Schema(
     },
     { timestamps: true }
 )
+
+const  Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
