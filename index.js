27c414bf99ddb4267002822ebdfb47dbbb87a1a0
angep73 <p.umunyana@alustudent.com>
2025-03-21 13:22:59 +0200

    adding new functionalities

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const productRoute = require ("./routes/product.route")
 const authRoutes = require("./routes/auth.route");
-
+const cookieParser = require("cookie-parser")
 
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser.extend());
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
