fb75db5ed0fd81ff9a78022123a4f494aa97c389
angep73 <p.umunyana@alustudent.com>
2025-03-23 16:11:21 +0200

    adding the check user

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const productRoute = require ("./routes/product.route")
 const authRoutes = require("./routes/auth.route");
 const cookieParser = require("cookie-parser")
-
+const {requireAuth} = require("./middleware/auth.middleware")
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,11 +27,12 @@ mongoose
 
 //Routes
 app.use("/api/products",productRoute)
-app.get('/' , (req,res)=>{res.render('home')});
+app.get('/',(req,res)=>{res.render('home')});
 app.get("/api/products/:id", productRoute);
 app.post("/api/products", productRoute);
 app.put("/api/products/:id", productRoute);
 app.delete("/api/products/:id", productRoute);
+app.get('/smoothies',requireAuth,(req, res) => res.render('smoothies'));
 
 //Authorization and authentication routes
 app.use(authRoutes);
