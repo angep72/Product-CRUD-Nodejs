30cf9e9f6c7b659bfe702f29f3ff8c0d84d4549c
angep73 <p.umunyana@alustudent.com>
2025-03-27 12:33:31 +0200

    adding new functionalities in .env

--- index.js
+++ index.js
@@ -1,61 +1,49 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const productRoute = require ("./routes/product.route")
+const productRoute = require("./routes/product.route");
 const authRoutes = require("./routes/auth.route");
-const cookieParser = require("cookie-parser")
-const {requireAuth, checkUser} = require("./middleware/auth.middleware")
+const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middleware/auth.middleware");
 const categoryRoutes = require('./routes/category.route');
 const inventoryRoutes = require('./routes/inventory');
-const filtered_Products = require('./controllers/product.controller')
-//Middlewares
 
-
-
+// Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Set EJS as the view engine and serve static files from the 'public' folder
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-
-//Database Connection
+// Database Connection using environment variable for Mongo URI
 mongoose
-  .connect(
-    "mongodb+srv://angep32:eVE7wc6TBEFy0AMs@summativeapi.6s4pw.mongodb.net/"
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
   })
-  .catch((err) => console.error("Could not connect to MongoDB", err));
+  .catch((err) => {
+    console.error('Could not connect to MongoDB', err);
+  });
 
+// Routes
+app.use("/api/products", productRoute);
+app.use("/api/category", categoryRoutes);
+app.use("/api/inventory", inventoryRoutes);
 
-//Routes
-app.use("/api/products",productRoute)
-
-// app.get("/api/products/:id", productRoute);
-// app.post("/api/products", productRoute);
-// app.put("/api/products/:id", productRoute);
-// app.delete("/api/products/:id", productRoute);
-
-
-// routes
+// Authenticated routes
 app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 
-//Routes
+// Auth Routes
 app.use(authRoutes);
-app.use("/api/products",productRoute)
-app.use("/api/category",categoryRoutes);
-app.use("/api/inventory",inventoryRoutes);
 
-// app.use('/api/productsi',filtered_Products);
-
-
-
-
-app.listen(3000, "localhost", () => {
+// Start the server
+app.listen(process.env.PORT || 3000, "localhost", () => {
   console.log("Server is running on port 3000");
 });
